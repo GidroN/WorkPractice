f4ece1bdfd575146aae02b51544450d738ab6736
Anton Illenberger <i@gidroonn.ru>
2023-04-28 12:35:11 +0400

    added an additionally solve for task3

--- Tasks/Task3.py
+++ Tasks/Task3.py
@@ -1,17 +1,37 @@
+from collections import Counter
+# Мое решение
 lst1 = ['bella', 'label', 'roller']
 lst2 = ['cool', 'lock', 'cook']
 
-dictionary = {}
-result = []  
+def common_letters(lst):
+    dictionary = {}
+    result = []  
 
-for i in lst1:
-    for j in i:
-        if j in dictionary.keys():
-            dictionary[j] += 1
-        else:
-            dictionary[j] = 1
-   
-for key, value in dictionary.items():
-    result.extend([key] * (value // len(lst1)))
+    for i in lst:
+        for j in i:
+            if j in dictionary.keys():
+                dictionary[j] += 1
+            else:
+                dictionary[j] = 1
+    
+    for key, value in dictionary.items():
+        result.extend([key] * (value // len(lst)))
 
-print(result)
+    return result
+
+# Не мое решение
+def common_letters_with_duplicates(lst):
+    # Создаем список для общих символов
+    common_chars = []
+
+    # Находим общие символы без дубликатов
+    unique_common_chars = set.intersection(*map(set, lst))
+    
+    for char in unique_common_chars:
+        # Находим минимальное количество вхождений символа в каждую строку
+        min_count = min([Counter(string)[char] for string in lst])
+        
+        # Добавляем символ в результат с учетом количества вхождений
+        common_chars.extend([char] * min_count)
+
+    return common_chars
